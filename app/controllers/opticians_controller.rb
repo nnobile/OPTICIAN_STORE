41361b44c9f0102b805756daea9b7c5d03122d7a
Nick Nobile <nickcnobile@gmail.com>
2021-02-14 16:31:40 -0500

    add sessions controller, view, and root route

--- app/controllers/opticians_controller.rb
+++ app/controllers/opticians_controller.rb
@@ -1,2 +1,13 @@
 class OpticiansController < ApplicationController
+
+    #loading the signup form
+    def new
+    end
+
+    #handling signup
+    def create
+    end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get '/signup' to: 'opticians#new'
-  get '/login' to: 'sessions#new'
-  post '/login' to: 'sessions#create'
+  root 'sessions#home'
+  get '/signup', to: 'opticians#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   resources :opticians
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
