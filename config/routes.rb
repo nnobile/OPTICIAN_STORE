307358f27cb6448fac289501e054703c3a2acda5
Nick Nobile <nickcnobile@gmail.com>
2021-02-14 16:18:30 -0500

    generate sessions controller, add login + signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/signup' to: 'opticians#new'
+  get '/login' to: 'sessions#new'
+  post '/login' to: 'sessions#create'
   resources :opticians
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
