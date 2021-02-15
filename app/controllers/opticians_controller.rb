978a5c01037c2465e033954b3d2d05a19721891e
Nick Nobile <nickcnobile@gmail.com>
2021-02-15 17:05:50 -0500

    build strong params and create action in optician model

--- app/controllers/opticians_controller.rb
+++ app/controllers/opticians_controller.rb
@@ -1,13 +1,29 @@
 class OpticiansController < ApplicationController
 
-    #loading the signup form
+    # Loading the signup form
     def new
         @optician = Optician.new
     end
 
-    #handling signup
+    # Handling signup
     def create
+        @optician = Optician.new(optician_params)
+        if @optician.save
+            #login the user
+            session[:optician_id] = @optician.optician_id
+            #redirect_to patient_path
+        else
+            render :new
+        end
     end
+
+    
+    private
+
+    def optician_params
+        params.require(:optician).permit(:username, :email, :password)
+    end
+
 
 
 
