2e74726b29d6e9819638f5917a944a877427600c
Nick Nobile <nickcnobile@gmail.com>
2021-02-15 16:51:35 -0500

    add initial validations for username and email in optician model

--- app/models/optician.rb
+++ app/models/optician.rb
@@ -1,5 +1,6 @@
 class Optician < ApplicationRecord
     has_secure_password
     has_many :patients
-    
+    validates :username, :email, presence: true
+    validates :username, :email, uniqueness: true
 end
