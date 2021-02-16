06b1d2e59b9b890f1ded194c76ac7e2d174b881f
Nick Nobile <nickcnobile@gmail.com>
2021-02-15 21:16:28 -0500

    add create acvtion in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,15 @@ class SessionsController < ApplicationController
     end
 
     def create
+        @optician = Optician.find_by(username: params[:optician][:username])
+        if @optician && @optician.authenticate(params[:optician][:password])
+            session[:optician_id] = @optician.id
+            redirect_to patients_path
+        else
+            redirect_to 'login'
+        end
     end
+
 
     def home
     end
