0d77030a04cc428a46b7f865a9fb48ca467511fe
Nick Nobile <nickcnobile@gmail.com>
2021-02-17 18:35:02 -0500

    build current_user helper method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+
+    private 
+
+    def current_user
+        @current_user ||= Optician.find_by(:id => session[:optician_id]) if session[:optician_id]
+    end
+
 end
