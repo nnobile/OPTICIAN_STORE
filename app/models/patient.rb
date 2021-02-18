93735ca4303c4a1e0dfbcacdb1c1e9ce645bc28e
Nick Nobile <nickcnobile@gmail.com>
2021-02-17 22:51:51 -0500

    build initial patient + revenue helper method in patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,5 +3,10 @@ class Patient < ApplicationRecord
     belongs_to :optician
 
 
-    
+    def patient_and_total_revenue
+        "#{self.first_name}, #{self.last_name} -- Total Revenue = #{self.total_revenue}"
+    end
+
+        
+
 end
