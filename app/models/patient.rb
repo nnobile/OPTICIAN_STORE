50d7accb6bff040d1353b212d0430c4c2af6d334
Nick Nobile <nickcnobile@gmail.com>
2021-02-18 22:33:26 -0500

    add validations for patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,16 +1,16 @@
 class Patient < ApplicationRecord
     belongs_to :optometrist
     belongs_to :optician
+    validates :first_name, :last_name, presence: true
+    validates_uniqueness_of :email, :phone_number
 
 
-    def patient_and_total_revenue
-        "#{self.first_name}, #{self.last_name} -- Total Revenue = #{self.total_revenue}"
-    end
+    # def patient_and_total_revenue
+    #     "#{self.first_name}, #{self.last_name} -- Total Revenue = #{self.total_revenue}"
+    # end
 
-    def display_total_revenue 
-    end
-
-
+    # def display_total_revenue 
+    # end
 
         
 
