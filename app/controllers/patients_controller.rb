41338d1979d1e380c7a210ae769b0656c89a4356
Nick Nobile <nickcnobile@gmail.com>
2021-02-21 16:44:39 -0500

    revisions to patient controller logic

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,37 +2,39 @@ class PatientsController < ApplicationController
 before_action :check_for_logged_in #, except: [:index]
 
     def index
+        if params[:optician_id] && optician = Optician.find_by_id(params[:optician_id])
         @patients = Patient.order(:last_name)
     end
 
     def show
-        @patient = Patient.find_by(id: params[:id])
+        set_patient
     end
 
     def new
-        if params[:optician_id] && optician = Optician.find_by_if(params[:optician_id])
+        if params[:optician_id] && optician = Optician.find_by_id(params[:optician_id])
             @patient = optician.patients.build
         else
             @patient = Patient.new
             @patient.build_optician
-
+        end
     end
 
     def create
-        if logged_in?
-            @patient = Patient.new(optician_id: params[:optician_id])
-            @patient.save
-            redirect_to optician_patients_path
+        @patient = current_user.patients.build(patient_params)
+        if @patient.save
+        redirect_to patient_path(@patient)
         else
+            @patient.build_optician unless @patient.optician
             render :new
         end
     end
 
     def edit
-        @patient = Patient.find_by(id: params[:id]) 
+        set_patient 
     end
 
     def update
+        set_patient
         if @patient.update(patient_params)
             redirect_to edit_patient_path(@patient)
         else
@@ -41,22 +43,23 @@ before_action :check_for_logged_in #, except: [:index]
     end
 
     def destroy
+        set_patient
         @patient.destroy
-        redirect_to patient_path
+        redirect_to patients_path
     end
 
 
 private
 
-    # def set_patient
-    #     @patient = Patient.find_by(id: params[:id])
-    #     if !@patient
-    #     redirect_to optometrist_path
-    #     end
-    # end
+    def set_patient
+        @patient = Patient.find_by(id: params[:id])
+        if !@patient
+            redirect_to patients_path
+        end
+    end
 
     def patient_params
-        params.require(:patient).permit(:first_name, :last_name, :phone_number, :email, :address, :birthdate, :total_revenue, :notes)
+        params.require(:patient).permit(:first_name, :last_name, :phone_number, :email, :address, :birthdate, :total_revenue, :notes, :optician_id)
     end
 
 end
