fd4be9f0dbdaf9c46a5ff3a74320f2dec786a903
Nick Nobile <nickcnobile@gmail.com>
2021-03-10 23:15:41 -0500

    udpated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
   get '/patients/abc', to: 'patients#abc'
   resources :patients
-  resources :optometrists
   root 'sessions#home'
   get '/signup', to: 'opticians#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/auth/:provider/callback', to: 'sessions#omniauth'
-  resources :opticians do
+  resources :optometrists do
     resources :patients, only: [:create, :index, :new, :show]
-    resources :optometrists, only: [:create, :new, :index]
   end
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
